c37ae5a3c560498c9219d31a63dcf3d0335eb6cc
Shivani M <lakshmishivani2003@gmail.com>
2025-01-26 22:52:35 +0530

    code with toppicks,h&nav files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Categories from "./Components/CategoryList";
-import Items from "./Components/Items";
-import Wishlist from "./Components/wishlist";
-import ItemDetails from "./Components/Itemdetails";
+import Categories from "./components/CategoryList";
+import Items from "./components/Items";
+import Wishlist from "./components/wishlist";
+import ItemDetails from "./components/Itemdetails";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 function App() {
   return (
+    
     <BrowserRouter>
+      <Navbar/>
       <ToastContainer />
       <Routes>
+        <Route path = "/home" element = {<Home/>}/>
         <Route path="/" element={<Categories />} />
         <Route path="/category/:type" element={<Items />} />
         <Route path="/wishlist" element={<Wishlist />} />
