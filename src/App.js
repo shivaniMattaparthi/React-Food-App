78c608f99c2e3b16041df8b1141a8498d0d56c3b
ThippireddyAkhila <akhilathippireddy2004@gmail.com>
2025-01-26 22:16:41 +0530

    Top picks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Categories from "./Components/CategoryList";
+import Items from "./Components/Items";
+import Wishlist from "./Components/wishlist";
+import ItemDetails from "./Components/Itemdetails";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
-    <div className="text-red-300 font-semibold text-6xl">
-      <p>Hi hello world I,m shivani t</p>
-    </div>
-  )
+    <BrowserRouter>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Categories />} />
+        <Route path="/category/:type" element={<Items />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/details/:id" element={<ItemDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
